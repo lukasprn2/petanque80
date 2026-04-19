d78e5bd485f746aa05107064ad884f335c2855cb
ab <lukasperin.80@gmail.com>
2026-04-19 10:01:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 // Change cette valeur à chaque déploiement pour déclencher la MAJ
-const CACHE_VERSION = '2026-04-05-36';
+const CACHE_VERSION = '2026-04-05-37';
 const CACHE_NAME = 'petanque-boves-' + CACHE_VERSION;
 
 const ASSETS = [
